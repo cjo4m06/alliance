9239ba8a580fb60a87cc709a5279adbfdefee40e
cjo4m06 <e5717454@gmail.com>
2018-05-15 15:10:20 +0800

    新增 中文語系

---
+++ resources/lang/zh-TW/attr.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'work' => [
+
+        'type' => [
+            'open' => '營業時間',
+            'delivery' => '外送時間',
+        ],
+
+    ],
+
+    'pay' => [
+
+        'type' => [
+            'cash' => '現金',
+            'aipei' => '愛貝',
+            'line-pay' => 'LINE Pay',
+            'gama' => '橘子支',
+            'neweb' => '藍新',
+        ],
+
+    ],
+
+];
---
+++ resources/lang/zh-TW/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'local' => '本機開發',
+
+    'develop' => '開發環境',
+
+    'production' => '正式環境',
+
+];

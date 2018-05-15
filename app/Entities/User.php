e607e5059deee63069e299cc34b4b6cde5084160
cjo4m06 <e5717454@gmail.com>
2018-05-15 12:27:41 +0800

    init project

---
+++ app/Entities/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'line_id', 'guild_id', 'account', 'password', 'name', 'is_manager', 'is_active'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
